030248fe6c7f5f48df8b238f88e504725f5ad615
Peter Esser <me@peter-esser.de>
2024-08-20 18:45:14 +0200

    tests: add first working tests

---
+++ applications/rpg_table_helper/test/components/styled_box_test.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:rpg_table_helper/components/styled_box.dart';
+import 'package:rpg_table_helper/services/dependency_provider.dart';
+
+import '../test_configuration.dart';
+
+void main() {
+  group('StyledBox rendering', () {
+    testConfigurations(
+      disableLocals: true,
+      widgetName: 'StyledBox solo',
+      useMaterialAppWrapper: true,
+      screenFactory: (Locale locale) => StyledBox(
+        child: Container(
+          height: 20,
+          width: 20,
+          color: Colors.green,
+        ),
+      ),
+      getTestConfigurations: (Widget widgetToTest) => Map.fromEntries([
+        MapEntry(
+          'default',
+          DependencyProvider.getMockedDependecyProvider(
+            Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  widgetToTest,
+                ],
+              ),
+            ),
+          ),
+        ),
+        MapEntry(
+          'expanded',
+          DependencyProvider.getMockedDependecyProvider(
+            Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(child: widgetToTest),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ]),
+    );
+  });
+}
--- applications/rpg_table_helper/test/test_configuration.dart
+++ applications/rpg_table_helper/test/test_configuration.dart
@@ -1,149 +1,140 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_localizations/flutter_localizations.dart';
-// import 'package:flutter_localized_locales/flutter_localized_locales.dart';
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:golden_toolkit/golden_toolkit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
 
-// const testDevices = [
-//   Device(
-//     name: 'android 9to16',
-//     size: Size(43 * 9, 43 * 16),
-//     devicePixelRatio: 3,
-//     textScale: 1.0,
-//     safeArea: EdgeInsets.fromLTRB(0.0, 59.0, 0.0, 60.0),
-//   ),
-//   Device(
-//     // Lisas device
-//     name: 'samsungA34',
-//     size: Size(1080 / 2.0, 2340 / 2.0),
-//     devicePixelRatio: 3,
-//     textScale: 1.0,
-//     safeArea: EdgeInsets.fromLTRB(0.0, 59.0, 0.0, 60.0),
-//   ),
-//   Device(
-//     name: 'pixel 7 pro',
-//     size: Size(886 / 2.0, 1929 / 2.0),
-//     devicePixelRatio: 2,
-//     textScale: 1.0,
-//     safeArea: EdgeInsets.fromLTRB(0.0, 60.0, 0.0, 0.0),
-//   ),
-//   Device(
-//     name: 'iPhone 15',
-//     size: Size(1179 / 3.0, 2556 / 3.0),
-//     devicePixelRatio: 3,
-//     textScale: 1.0,
-//     safeArea: EdgeInsets.fromLTRB(0.0, 59.0, 0.0, 60.0),
-//   ),
-//   Device(
-//     name: 'iPhone 15 Pro Max',
-//     size: Size(1290 / 3.0, 2796 / 3.0),
-//     devicePixelRatio: 3,
-//     safeArea: EdgeInsets.fromLTRB(0.0, 59.0, 0.0, 60.0),
-//     textScale: 1.0,
-//   ),
-//   Device(
-//     name: 'iPhone SE (3rd gen)',
-//     size: Size(750 / 2, 1334 / 2),
-//     devicePixelRatio: 2,
-//     safeArea: EdgeInsets.fromLTRB(0.0, 20.0 / 2, 0.0, 0.0),
-//     textScale: 1.0,
-//   ),
-//   Device(
-//     name: 'super high iPhone',
-//     size: Size(1290 / 3.0, 2796 / 3.0 * 5.0),
-//     devicePixelRatio: 3,
-//     safeArea: EdgeInsets.fromLTRB(0.0, 59.0 / 3.0, 0.0, 34.0 / 3.0),
-//     textScale: 1.0,
-//   ),
-// ];
+const testDevices = [
+  Device(
+    name: 'android 9to16',
+    size: Size(43 * 9, 43 * 16),
+    devicePixelRatio: 3,
+    textScale: 1.0,
+    safeArea: EdgeInsets.fromLTRB(0.0, 59.0, 0.0, 60.0),
+  ),
+  Device(
+    // Lisas device
+    name: 'samsungA34',
+    size: Size(1080 / 2.0, 2340 / 2.0),
+    devicePixelRatio: 3,
+    textScale: 1.0,
+    safeArea: EdgeInsets.fromLTRB(0.0, 59.0, 0.0, 60.0),
+  ),
+  Device(
+    name: 'pixel 7 pro',
+    size: Size(886 / 2.0, 1929 / 2.0),
+    devicePixelRatio: 2,
+    textScale: 1.0,
+    safeArea: EdgeInsets.fromLTRB(0.0, 60.0, 0.0, 0.0),
+  ),
+  Device(
+    name: 'iPhone 15',
+    size: Size(1179 / 3.0, 2556 / 3.0),
+    devicePixelRatio: 3,
+    textScale: 1.0,
+    safeArea: EdgeInsets.fromLTRB(0.0, 59.0, 0.0, 60.0),
+  ),
+  Device(
+    name: 'iPhone 15 Pro Max',
+    size: Size(1290 / 3.0, 2796 / 3.0),
+    devicePixelRatio: 3,
+    safeArea: EdgeInsets.fromLTRB(0.0, 59.0, 0.0, 60.0),
+    textScale: 1.0,
+  ),
+  Device(
+    name: 'iPhone SE (3rd gen)',
+    size: Size(750 / 2, 1334 / 2),
+    devicePixelRatio: 2,
+    safeArea: EdgeInsets.fromLTRB(0.0, 20.0 / 2, 0.0, 0.0),
+    textScale: 1.0,
+  ),
+  Device(
+    name: 'super high iPhone',
+    size: Size(1290 / 3.0, 2796 / 3.0 * 5.0),
+    devicePixelRatio: 3,
+    safeArea: EdgeInsets.fromLTRB(0.0, 59.0 / 3.0, 0.0, 34.0 / 3.0),
+    textScale: 1.0,
+  ),
+];
 
-// void testConfigurations({
-//   required Widget Function(Locale locale) screenFactory,
-//   required Map<String, Widget> Function(Widget widgetToTest)
-//       getTestConfigurations,
-//   required String widgetName,
-//   bool disableLocals = false,
-//   Future<void> Function(WidgetTester tester, Locale local)? testerInteractions,
-//   bool useMaterialAppWrapper = true,
-// }) {
-//   Widget? widgetToTest;
-//   var supportedLocales = const AppLocalizationDelegate().supportedLocales;
-//   if (disableLocals) {
-//     supportedLocales = [supportedLocales[0]];
-//   }
-//   for (var local in supportedLocales) {
-//     if (useMaterialAppWrapper) {
-//       widgetToTest = ProviderScope(
-//         child: MaterialApp(
-//           localizationsDelegates: const [
-//             S.delegate,
-//             GlobalMaterialLocalizations.delegate,
-//             GlobalWidgetsLocalizations.delegate,
-//             GlobalCupertinoLocalizations.delegate,
-//           ],
-//           locale: local,
-//           supportedLocales: const AppLocalizationDelegate().supportedLocales,
-//           debugShowCheckedModeBanner: false,
-//           debugShowMaterialGrid: false,
-//           themeMode: ThemeMode.dark,
-//           title: 'TriviaCrusher',
-//           theme: ThemeData.dark(useMaterial3: true),
-//           builder: (BuildContext context, Widget? child) {
-//             // Set a custom screen size for the test
-//             return screenFactory(local);
-//           },
-//         ),
-//       );
-//     } else {
-//       widgetToTest = screenFactory(local);
-//     }
+void testConfigurations({
+  required Widget Function(Locale locale) screenFactory,
+  required Map<String, Widget> Function(Widget widgetToTest)
+      getTestConfigurations,
+  required String widgetName,
+  bool disableLocals = false,
+  Future<void> Function(WidgetTester tester, Locale local)? testerInteractions,
+  bool useMaterialAppWrapper = true,
+}) {
+  Widget? widgetToTest;
+  var supportedLocales = AppLocalizations.supportedLocales;
+  if (disableLocals) {
+    supportedLocales = [supportedLocales[0]];
+  }
+  for (var local in supportedLocales) {
+    if (useMaterialAppWrapper) {
+      widgetToTest = MaterialApp(
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        locale: local,
+        supportedLocales: AppLocalizations.supportedLocales,
+        debugShowCheckedModeBanner: false,
+        debugShowMaterialGrid: false,
+        themeMode: ThemeMode.dark,
+        title: 'TriviaCrusher',
+        theme: ThemeData.dark(useMaterial3: true),
+        builder: (BuildContext context, Widget? child) {
+          // Set a custom screen size for the test
+          return screenFactory(local);
+        },
+      );
+    } else {
+      widgetToTest = screenFactory(local);
+    }
 
-//     var counter = 1;
+    var counter = 1;
 
-//     for (var widgetConfig in getTestConfigurations(widgetToTest).entries) {
-//       var testName =
-//           '$counter - $widgetName (Language ${local.languageCode}, ${widgetConfig.key})';
-//       counter++;
+    for (var widgetConfig in getTestConfigurations(widgetToTest).entries) {
+      var testName =
+          '$counter - $widgetName (Language ${local.languageCode}, ${widgetConfig.key})';
+      counter++;
 
-//       testGoldens(testName, (WidgetTester tester) async {
-//         TestWidgetsFlutterBinding.ensureInitialized();
-//         await loadAppFonts();
-//         await loadAppFonts();
-//         await tester.pumpAndSettle();
-//         await loadAppFonts();
-//         await tester.pumpAndSettle();
+      testGoldens(testName, (WidgetTester tester) async {
+        TestWidgetsFlutterBinding.ensureInitialized();
+        await loadAppFonts();
+        await loadAppFonts();
+        await tester.pumpAndSettle();
+        await loadAppFonts();
+        await tester.pumpAndSettle();
 
-//         await tester.pumpWidgetBuilder(
-//           Builder(builder: (context) {
-//             return Localizations(
-//               delegates: const [
-//                 LocaleNamesLocalizationsDelegate(),
-//                 S.delegate,
-//                 GlobalMaterialLocalizations.delegate,
-//                 GlobalWidgetsLocalizations.delegate,
-//                 GlobalCupertinoLocalizations.delegate,
-//               ],
-//               locale: local,
-//               child: Localizations.override(
-//                 context: context,
-//                 locale: local,
-//                 child: widgetConfig.value,
-//               ),
-//             );
-//           }),
-//         );
+        await tester.pumpWidgetBuilder(
+          Builder(builder: (context) {
+            return Localizations(
+              delegates: const [
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate,
+              ],
+              locale: local,
+              child: Localizations.override(
+                context: context,
+                locale: local,
+                child: widgetConfig.value,
+              ),
+            );
+          }),
+        );
 
-//         if (testerInteractions != null) {
-//           await testerInteractions(tester, local);
-//         }
-//         await loadAppFonts();
+        if (testerInteractions != null) {
+          await testerInteractions(tester, local);
+        }
+        await loadAppFonts();
 
-//         await multiScreenGolden(
-//           tester,
-//           '../../goldens/$widgetName/$testName',
-//           devices: testDevices,
-//         );
-//       });
-//     }
-//   }
-// }
+        await multiScreenGolden(
+          tester,
+          '../../goldens/$widgetName/$testName',
+          devices: testDevices,
+        );
+      });
+    }
+  }
+}
