9f0cf3cd436aafb03f888a1de5924a55adc4cfdf
Peter Esser <me@peter-esser.de>
2024-09-04 15:52:46 +0200

    chore: move some files

--- applications/rpg_table_helper/lib/screens/wizards/all_wizard_configurations.dart
+++ applications/rpg_table_helper/lib/screens/wizards/all_wizard_configurations.dart
@@ -1,10 +1,10 @@
 import 'package:rpg_table_helper/components/wizards/wizard_renderer_for_configuration.dart';
-import 'package:rpg_table_helper/screens/wizards/rpg_configuration_wizard_step_1_campagne_name.dart';
-import 'package:rpg_table_helper/screens/wizards/rpg_configuration_wizard_step_2_character_configurations_preset.dart';
-import 'package:rpg_table_helper/screens/wizards/rpg_configuration_wizard_step_3_currency_definition.dart';
-import 'package:rpg_table_helper/screens/wizards/rpg_configuration_wizard_step_4_item_locations.dart';
-import 'package:rpg_table_helper/screens/wizards/rpg_configuration_wizard_step_5_item_categories.dart';
-import 'package:rpg_table_helper/screens/wizards/rpg_configuration_wizard_step_6_items.dart';
+import 'package:rpg_table_helper/screens/wizards/rpg_configuration_wizard/rpg_configuration_wizard_step_1_campagne_name.dart';
+import 'package:rpg_table_helper/screens/wizards/rpg_configuration_wizard/rpg_configuration_wizard_step_2_character_configurations_preset.dart';
+import 'package:rpg_table_helper/screens/wizards/rpg_configuration_wizard/rpg_configuration_wizard_step_3_currency_definition.dart';
+import 'package:rpg_table_helper/screens/wizards/rpg_configuration_wizard/rpg_configuration_wizard_step_4_item_locations.dart';
+import 'package:rpg_table_helper/screens/wizards/rpg_configuration_wizard/rpg_configuration_wizard_step_5_item_categories.dart';
+import 'package:rpg_table_helper/screens/wizards/rpg_configuration_wizard/rpg_configuration_wizard_step_6_items.dart';
 
 Map<String, WizardConfiguration> allWizardConfigurations = {
   "/rpgconfigurationwizard": WizardConfiguration(
