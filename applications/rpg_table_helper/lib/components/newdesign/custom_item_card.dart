4230d7a0ffc0f2792d52735f18e60ede109072ff
Peter Esser <me@peter-esser.de>
2024-11-02 21:20:53 +0100

    fix: minor ui artefact

--- applications/rpg_table_helper/lib/components/newdesign/custom_item_card.dart
+++ applications/rpg_table_helper/lib/components/newdesign/custom_item_card.dart
@@ -383,16 +383,20 @@ class GetBorderCornerStone extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Align(
-      alignment: alignment,
-      child: Transform.translate(
-        offset: Offset(alignment.x * 7 * scalar, alignment.y * 7 * scalar),
-        child: Transform.rotate(
-          angle: 0.785398163,
-          child: Container(
-            width: 25 * scalar,
-            height: 25 * scalar,
-            color: backgroundColor,
+    return Padding(
+      padding: const EdgeInsets.all(1.0),
+      child: Align(
+        alignment: alignment,
+        child: Transform.translate(
+          offset: Offset(alignment.x * 7 * scalar, alignment.y * 7 * scalar),
+          child: Transform.rotate(
+            angle: 0.785398163,
+            child: Container(
+              width: 25 * scalar,
+              height: 25 * scalar,
+              padding: EdgeInsets.all(5),
+              child: Container(color: backgroundColor),
+            ),
           ),
         ),
       ),
