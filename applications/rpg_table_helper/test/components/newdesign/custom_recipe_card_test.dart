ef0559b42c5f21deb18a94a066c8a653249523ef
Peter Esser <me@peter-esser.de>
2024-11-03 23:39:08 +0100

    feat: add recipie cards

---
+++ applications/rpg_table_helper/test/components/newdesign/custom_recipe_card_test.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:rpg_table_helper/components/newdesign/custom_item_card.dart';
+import 'package:rpg_table_helper/constants.dart';
+import 'package:rpg_table_helper/services/dependency_provider.dart';
+
+import '../../test_configuration.dart';
+
+void main() {
+  group('CustomRecipeCard rendering', () {
+    var recipeCards = [
+      // display placeholder image
+      CustomRecipeCard(
+        title: "Small Healing Potion",
+        requirements: [],
+        ingedients: [
+          CustomRecipeCardItemPair(amount: 2, itemName: "Roter Fuchsschwanz"),
+          CustomRecipeCardItemPair(amount: 1, itemName: "Blüte der Nacht"),
+          CustomRecipeCardItemPair(amount: 2, itemName: "Mondgrass"),
+          CustomRecipeCardItemPair(amount: 2, itemName: "Dunkelflechte"),
+        ],
+      ),
+
+      // test test asset image override
+      CustomRecipeCard(
+        title: "Small Healing Potion",
+        imageUrl: "assets/images/itemhealingpotion.png",
+        requirements: [],
+        ingedients: [
+          CustomRecipeCardItemPair(amount: 2, itemName: "Roter Fuchsschwanz"),
+          CustomRecipeCardItemPair(amount: 1, itemName: "Blüte der Nacht"),
+          CustomRecipeCardItemPair(amount: 2, itemName: "Mondgrass"),
+          CustomRecipeCardItemPair(amount: 2, itemName: "Dunkelflechte"),
+        ],
+      ),
+
+      Container(
+        width: 100,
+        height: 100,
+        color: Colors.orange,
+        child: QuarterCircleCutout(
+            circleOuterDiameter: 50.0,
+            circleDegrees: 45,
+            circleColor: bgColor,
+            circleInnerDiameter: 40.0,
+            cicleInnerColor: darkColor),
+      ),
+
+      CustomRecipeCard(
+        title: "Small Healing Potion",
+        imageUrl: "assets/images/itemhealingpotion.png",
+        requirements: [],
+        ingedients: [
+          CustomRecipeCardItemPair(amount: 2, itemName: "Roter Fuchsschwanz"),
+          CustomRecipeCardItemPair(amount: 2, itemName: "Roter Fuchsschwanz"),
+          CustomRecipeCardItemPair(amount: 2, itemName: "Roter Fuchsschwanz"),
+          CustomRecipeCardItemPair(amount: 1, itemName: "Blüte der Nacht"),
+          CustomRecipeCardItemPair(amount: 2, itemName: "Mondgrass"),
+          CustomRecipeCardItemPair(amount: 2, itemName: "Dunkelflechte"),
+        ],
+      ),
+
+      CustomRecipeCard(
+        title: "Small Healing Potion",
+        imageUrl: "assets/images/itemhealingpotion.png",
+        requirements: [
+          CustomRecipeCardItemPair(amount: 2, itemName: "Roter Fuchsschwanz"),
+          CustomRecipeCardItemPair(amount: 2, itemName: "Roter Fuchsschwanz"),
+        ],
+        ingedients: [
+          CustomRecipeCardItemPair(amount: 2, itemName: "Roter Fuchsschwanz"),
+          CustomRecipeCardItemPair(amount: 1, itemName: "Blüte der Nacht"),
+        ],
+      ),
+
+      CustomRecipeCard(
+        title: "Small Healing Potion",
+        imageUrl: "assets/images/itemhealingpotion.png",
+        requirements: [
+          CustomRecipeCardItemPair(amount: 2, itemName: "Roter Fuchsschwanz"),
+          CustomRecipeCardItemPair(amount: 2, itemName: "Roter Fuchsschwanz"),
+        ],
+        ingedients: [
+          CustomRecipeCardItemPair(amount: 2, itemName: "Roter Fuchsschwanz"),
+          CustomRecipeCardItemPair(amount: 1, itemName: "Blüte der Nacht"),
+          CustomRecipeCardItemPair(amount: 2, itemName: "Mondgrass"),
+          CustomRecipeCardItemPair(amount: 2, itemName: "Dunkelflechte"),
+        ],
+      ),
+    ];
+
+    for (var i = 0; i < recipeCards.length; i++) {
+      var itemcard = recipeCards[i];
+      testConfigurations(
+        disableLocals: true,
+        widgetName: 'CustomRecipeCard$i',
+        pathPrefix: "../",
+        useMaterialAppWrapper: true,
+        screenFactory: (Locale locale) => SizedBox(
+          width: 300,
+          height: 600,
+          child: itemcard,
+        ),
+        getTestConfigurations: (Widget widgetToTest) => Map.fromEntries([
+          MapEntry(
+            'default',
+            DependencyProvider.getMockedDependecyProvider(
+              child: Center(
+                  child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  itemcard,
+                ],
+              )),
+            ),
+          ),
+        ]),
+      );
+    }
+  });
+}
