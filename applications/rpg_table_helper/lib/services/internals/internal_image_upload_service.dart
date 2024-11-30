ff72c10e186c8b4907faaa0260df8bb7342377a6
Peter Esser <me@peter-esser.de>
2024-11-30 12:52:16 +0100

    fix: auto build step

--- applications/rpg_table_helper/lib/services/internals/internal_image_upload_service.dart
+++ applications/rpg_table_helper/lib/services/internals/internal_image_upload_service.dart
@@ -1,8 +1,6 @@
 import 'package:chopper/chopper.dart';
 import 'package:http/http.dart' as http;
 
-part 'internal_image_upload_service.chopper.dart';
-
 @ChopperApi()
 abstract class InternalImageUploadService extends ChopperService {
   @Post(path: '/Image/streamimageupload')
@@ -14,4 +12,40 @@ abstract class InternalImageUploadService extends ChopperService {
 
   static InternalImageUploadService create([ChopperClient? client]) =>
       _$InternalImageUploadService(client);
+}
+
+final class _$InternalImageUploadService extends InternalImageUploadService {
+  _$InternalImageUploadService([ChopperClient? client]) {
+    if (client == null) return;
+    this.client = client;
+  }
+
+  @override
+  final Type definitionType = InternalImageUploadService;
+
+  @override
+  Future<Response<String>> uploadImage(
+    String campagneId,
+    http.MultipartFile image,
+  ) {
+    final Uri $url = Uri.parse('/Image/streamimageupload');
+    final Map<String, dynamic> $params = <String, dynamic>{
+      'campagneId': campagneId
+    };
+    final List<PartValue> $parts = <PartValue>[
+      PartValueFile<http.MultipartFile>(
+        'image',
+        image,
+      )
+    ];
+    final Request $request = Request(
+      'POST',
+      $url,
+      client.baseUrl,
+      parts: $parts,
+      multipart: true,
+      parameters: $params,
+    );
+    return client.send<String, String>($request);
+  }
 }
