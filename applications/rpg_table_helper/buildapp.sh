0194c4294fc9c0473d6dad0db5c121f9cd2fb393
Peter Esser <me@peter-esser.de>
2025-01-21 18:57:38 +0100

    chore: update Node.js version and comment out build commands in scripts

--- applications/rpg_table_helper/buildapp.sh
+++ applications/rpg_table_helper/buildapp.sh
@@ -4,7 +4,7 @@ start=`date +%s`
 
 dart run build_runner build
 
-flutter test --coverage
+# flutter test --coverage
 
 python3 versionmanager.py
 
@@ -21,18 +21,18 @@ sleep 6
 # This resolves a bug if you'd change the apps name after building the ipa once it does not auto upload afterwards anymore...
 rm -rf build/ios/ipa/
 
-flutter build ipa --no-tree-shake-icons --obfuscate --split-debug-info=./obfuscation/$version/ipa/
+# flutter build ipa --no-tree-shake-icons --obfuscate --split-debug-info=./obfuscation/$version/ipa/
 # open build/ios/archive/Runner.xcarchive/
 # create certs here: https://appstoreconnect.apple.com/access/api
 sleep 6
 
-xcrun altool --upload-app --type ios -f build/ios/ipa/*.ipa --apiIssuer c97ca5fd-1e2f-4be2-8d67-01078f794b3d --apiKey RQXKQ9835X --show-progress
+# xcrun altool --upload-app --type ios -f build/ios/ipa/*.ipa --apiIssuer c97ca5fd-1e2f-4be2-8d67-01078f794b3d --apiKey RQXKQ9835X --show-progress
 
 sleep 6
 
 # open ios/Runner.xcworkspace
 
-flutter build appbundle --no-tree-shake-icons --no-shrink --obfuscate --split-debug-info=./obfuscation/$version/appbundle/
+# flutter build appbundle --no-tree-shake-icons --no-shrink --obfuscate --split-debug-info=./obfuscation/$version/appbundle/
 
 git add obfuscation/*
 git commit -m "chore: Checkin symbols for ${version}"
