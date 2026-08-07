920e19c9ba37592647bc66519d1fc2232722795b
Peter Esser <me@peter-esser.de>
2026-08-07 19:47:33 +0200

    test(skins): add DM page goldens for both new skins

---
+++ applications/rpg_table_helper/test/screens/pageviews/dm_page_screen_night_cartographer_test.dart
@@ -0,0 +1,231 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:quest_keeper/generated/l10n.dart';
+import 'package:quest_keeper/generated/swaggen/swagger.models.swagger.dart';
+import 'package:quest_keeper/helpers/character_sheet_skins/character_sheet_skin.dart';
+import 'package:quest_keeper/helpers/connection_details_provider.dart';
+import 'package:quest_keeper/helpers/rpg_configuration_provider.dart';
+import 'package:quest_keeper/l10n/app_localizations.dart';
+import 'package:quest_keeper/main.dart';
+import 'package:quest_keeper/models/connection_details.dart';
+import 'package:quest_keeper/models/rpg_character_configuration.dart';
+import 'package:quest_keeper/models/rpg_configuration_model.dart';
+import 'package:quest_keeper/screens/pageviews/dm_pageview/dm_page_screen.dart';
+import 'package:quest_keeper/services/custom_theme_provider.dart';
+import 'package:quest_keeper/services/dependency_provider.dart';
+import 'package:quest_keeper/services/note_documents_service.dart';
+
+import '../../custom_font_loader.dart';
+import '../../test_configuration.dart';
+
+/// iPad-landscape goldens for all DM pageview screens under Night Cartographer.
+/// English only — Cartographer is a dark night-atlas pack.
+void main() {
+  const testCases = [
+    (0, "dmcampagnemanagmentscreen"),
+    (1, "dmplayeroverviewscreen"),
+    (2, "dminitiativescreen"),
+    (3, "dmgrantitemsscreen"),
+    (4, "dmlorescreen"),
+    (5, "dmallgeneratedimagesscreen"),
+  ];
+
+  group('night cartographer dmpagescreens', () {
+    final navigatorKey = GlobalKey<NavigatorState>();
+
+    setUp(() {
+      MockNoteDocumentService.preferArcaneLedgerLoreFixtures = true;
+    });
+    tearDown(() {
+      MockNoteDocumentService.preferArcaneLedgerLoreFixtures = false;
+    });
+
+    for (final testcase in testCases) {
+      testConfigurations(
+        disableLocals: true,
+        disableDarkMode: true,
+        pathPrefix: "../",
+        overrideSkinId: CharacterSheetSkinIds.nightCartographer,
+        devices: testDevicesLedger,
+        widgetName:
+            'night-cartographer-dmpagescreens${testcase.$1}-${testcase.$2}',
+        useMaterialAppWrapper: true,
+        testerInteractions: (tester, local) async {
+          await tester.pumpAndSettle();
+          await customLoadAppFonts();
+          await customLoadAppFonts();
+          await tester.pumpAndSettle();
+          await customLoadAppFonts();
+          await tester.pumpAndSettle();
+        },
+        screenFactory: (Locale locale, Brightness brightnessToTest) =>
+            ProviderScope(
+          overrides: [
+            rpgConfigurationProvider.overrideWith((ref) {
+              return RpgConfigurationNotifier(
+                decks: AsyncValue.data(
+                  RpgConfigurationModel.getBaseConfiguration().copyWith(
+                    defaultSkinId: CharacterSheetSkinIds.nightCartographer,
+                  ),
+                ),
+                ref: ref,
+                runningInTests: true,
+              );
+            }),
+            connectionDetailsProvider.overrideWith((ref) {
+              return ConnectionDetailsNotifier(
+                initState: AsyncValue.data(ConnectionDetails.defaultValue()
+                    .copyWith(
+                        fightSequence: FightSequence(
+                            fightUuid: "f10526be-c69a-46be-8802-df9421e6187b",
+                            sequence: [
+                              (
+                                "575fb9d9-c2a0-47df-bec4-5de1b3d5ca4d",
+                                "Frodo",
+                                21
+                              ),
+                              (null, "Gegner #2", 17),
+                              (
+                                "0eff8827-14f1-46a1-8695-ef7dc5323137",
+                                "Gandalf",
+                                16
+                              ),
+                              (null, "Gegner #1", 15),
+                            ]),
+                        isConnected: true,
+                        isConnecting: true,
+                        isDm: true,
+                        campagneId: "51f263bc-37cf-44d4-90f3-87d656ae29df",
+                        isInSession: true,
+                        sessionConnectionNumberForPlayers: "123-321",
+                        lastGrantedItems: [
+                      GrantedItemsForPlayer(
+                          characterName: "Frodo",
+                          playerId: "fghjkl",
+                          grantedItems: []),
+                      GrantedItemsForPlayer(
+                        characterName: "Gandalf",
+                        playerId: "ghjiuhjkiujhn",
+                        grantedItems: [
+                          RpgCharacterOwnedItemPair(
+                            itemUuid:
+                                RpgConfigurationModel.getBaseConfiguration()
+                                    .allItems
+                                    .first
+                                    .uuid,
+                            amount: 2,
+                          ),
+                          RpgCharacterOwnedItemPair(
+                            itemUuid:
+                                RpgConfigurationModel.getBaseConfiguration()
+                                    .allItems[2]
+                                    .uuid,
+                            amount: 12,
+                          ),
+                        ],
+                      ),
+                    ],
+                        openPlayerRequests: [
+                      PlayerJoinRequests(
+                          playerName: "Thamior",
+                          username: "Tobias",
+                          campagneJoinRequestId: "asdf2",
+                          playerCharacterId: "asdf"),
+                      PlayerJoinRequests(
+                          playerName: "Kardan",
+                          username: "Peter",
+                          campagneJoinRequestId: "asdf2",
+                          playerCharacterId: "asdf"),
+                      PlayerJoinRequests(
+                          playerName: "Faugar",
+                          username: "Lukas",
+                          campagneJoinRequestId: "asdf2",
+                          playerCharacterId: "asdf"),
+                      PlayerJoinRequests(
+                          playerName: "Shava",
+                          username: "Rachel",
+                          campagneJoinRequestId: "asdf2",
+                          playerCharacterId: "asdf"),
+                    ],
+                        connectedPlayers: [
+                      OpenPlayerConnection(
+                        lastPing: DateTime(2025, 02, 26, 12, 00),
+                        userId: UserIdentifier(
+                            $value: "9a709402-5620-479c-85b7-718ae01e0a83"),
+                        playerCharacterId: PlayerCharacterIdentifier(
+                            $value: "575fb9d9-c2a0-47df-bec4-5de1b3d5ca4d"),
+                        configuration:
+                            RpgCharacterConfiguration.getBaseConfiguration(
+                                    variant: 0,
+                                    RpgConfigurationModel
+                                        .getBaseConfiguration())
+                                .copyWith(
+                                  characterName: "Gandalf",
+                                  skinId:
+                                      CharacterSheetSkinIds.nightCartographer,
+                                ),
+                      ),
+                    ])),
+                ref: ref,
+                runningInTests: true,
+              );
+            }),
+          ],
+          child: ThemeConfigurationForApp(
+            child: MaterialApp(
+                navigatorKey: navigatorKey,
+                debugShowCheckedModeBanner: false,
+                localizationsDelegates: [
+                  ...AppLocalizations.localizationsDelegates,
+                  S.delegate
+                ],
+                locale: locale,
+                supportedLocales: AppLocalizations.supportedLocales,
+                darkTheme: ThemeData.dark(),
+                themeMode: ThemeMode.dark,
+                theme: ThemeData(
+                  colorScheme:
+                      ColorScheme.fromSeed(seedColor: Colors.brown),
+                  fontFamily: 'Ruwudu',
+                  useMaterial3: true,
+                  iconTheme: const IconThemeData(
+                    color: Colors.white,
+                    size: 16,
+                  ),
+                ),
+                home: ThemeConfigurationForApp(
+                  child: CustomThemeProvider(
+                    overrideBrightness: Brightness.dark,
+                    overrideSkinId: CharacterSheetSkinIds.nightCartographer,
+                    child: Scaffold(
+                      resizeToAvoidBottomInset: false,
+                      body: Builder(builder: (context) {
+                        return DmPageScreen(
+                          startScreenOverride: testcase.$1,
+                        );
+                      }),
+                    ),
+                  ),
+                )),
+          ),
+        ),
+        getTestConfigurations: (Widget widgetToTest, Brightness brightness) =>
+            Map.fromEntries([
+          MapEntry(
+            'night_cartographer',
+            CustomThemeProvider(
+              overrideBrightness: Brightness.dark,
+              overrideSkinId: CharacterSheetSkinIds.nightCartographer,
+              child: DependencyProvider.getMockedDependecyProvider(
+                child: Center(
+                  child: widgetToTest,
+                ),
+              ),
+            ),
+          ),
+        ]),
+      );
+    }
+  });
+}
